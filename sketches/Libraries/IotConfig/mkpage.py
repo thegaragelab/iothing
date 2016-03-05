27ff473df535b8fe96464cdae332a5ed15850caa
Shane Gough <shane@thegaragelab.com>
2016-03-05 13:30:03 +1000

    Adding a configuration page to compile into the library.

---
+++ sketches/Libraries/IotConfig/mkpage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#----------------------------------------------------------------------------
+# 05-Mar-2016 ShaneG
+#
+# Simple utility to convert a text file into a C character array.
+#----------------------------------------------------------------------------
+import sys
+from os.path import splitext
+
+if __name__ == "__main__":
+  if len(sys.argv) <> 2:
+    print "You must specify a single filename on the command line."
+    exit(1)
+  # Read the data file
+  with open(sys.argv[1], "r") as input:
+    data = input.read()
+  # Generate the output
+  output = open(splitext(sys.argv[1])[0] + ".cpp", "w")
+  output.write("char CONFIG_PAGE[] = {\n  ")
+  count = 0
+  for ch in data:
+    output.write("0x%02x, " % ord(ch))
+    count = count + 1
+    if (count == 12):
+      count = 0
+      output.write("\n  ")
+  output.write("\n  };\n")
+  output.close()
+  
