61944ac11ba14d90645d2059d2894b540e35ac3c
Shane Gough <shane@thegaragelab.com>
2016-03-19 15:17:58 +1000

    Added EEPROM support and feedback on configuration mode.

--- sketches/Barebones/Barebones.ino
+++ sketches/Barebones/Barebones.ino
@@ -1,15 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include <EEPROM.h>
 #include <TGL.h>
 #include <Json.h>
 #include <IotConfig.h>
 
+bool inConfig = true;
+
 void setup() {
   Serial.begin(115200);
-  IotConfig.setup(false);
+  EEPROM.begin(IOTCONFIG_BLOCK_SIZE);
+  inConfig = IotConfig.setup(false);
   }
 
 void loop() {
   IotConfig.loop();
+  if(inConfig)
+    return; // Will restart when config finished
   }
